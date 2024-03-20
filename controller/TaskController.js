fa4b88e05831691f353ced2a1ef822092579c99d
Rafael Nagatomo <rafaelnagatomo@gmail.com>
2024-03-20 01:19:03 -0300

    feat: updateTask and deleteTask added

--- controller/TaskController.js
+++ controller/TaskController.js
@@ -3,7 +3,7 @@ const Task = require("../models/Task");
 const getAllTasks = async (req, res) => {
   try {
     const tasksList = await Task.find();
-    return res.render("index", { tasksList });
+    return res.render("index", { tasksList, task: null });
   } catch (err) {
     res.status(500).send({ error: err.message });
   }
@@ -24,7 +24,35 @@ const createTask = async (req, res) => {
   }
 };
 
+const getById = async (req, res) => {
+  const task = await Task.findOne({ _id: req.params.id });
+  const tasksList = await Task.find();
+  res.render("index", { task, tasksList });
+};
+
+const updateTask = async (req, res) => {
+  try {
+    const task = req.body;
+    await Task.updateOne({ _id: req.params.id }, task);
+    res.redirect("/");
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+};
+
+const deleteTask = async (req, res) => {
+  try {
+    await Task.deleteOne({ _id: req.params.id });
+    res.redirect("/");
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+};
+
 module.exports = {
   getAllTasks,
   createTask,
+  getById,
+  updateTask,
+  deleteTask,
 };
