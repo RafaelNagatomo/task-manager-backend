19b45cc661942893936701e0baa2a10da1aef181
Rafael Nagatomo <rafaelnagatomo@gmail.com>
2024-03-23 19:14:12 -0300

    return to express

--- index.js
+++ index.js
@@ -2,17 +2,26 @@ const express = require("express");
 const cors = require("cors");
 
 const routes = require("./routes/routes");
-const connectToDb = require("./database/db");
+//const connectToDb = require("./database/db");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-connectToDb();
+//connectToDb();
 
 app.use("/", routes);
 
+/* 
 const port = 3001;
 app.listen(port, () =>
   console.log(`Servidor rodando em http://localhost:${port}`)
 );
+
+*/
+
+const PORT = process.env.PORT || 5003;
+
+server.listen(PORT, () => {
+  console.log(`servidor rodando na URL http://${HOSTNAME}:${PORT}`);
+});
