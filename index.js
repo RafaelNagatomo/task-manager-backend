2a6e6107545e5f52160bcbbdbfded8acdf5713b3
Rafael Nagatomo <rafaelnagatomo@gmail.com>
2024-03-24 20:40:57 -0300

    feat: return to original

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require("express");
 const cors = require("cors");
 
 const routes = require("./routes/routes");
-//const connectToDb = require("./database/db");
+const connectToDb = require("./database/db");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-//connectToDb();
+connectToDb();
 
 app.use("/", routes);
 
